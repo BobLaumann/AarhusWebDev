36b0a0afef0a5353d6cc32031155afc1357c77dc
DESKTOP-165DMB0\Bob <bob.laumann1@gmail.com>
2018-03-29 10:22:23 +0200

    First Commit

---
+++ AarhusWebDevCoop2/ViewModels/ContactForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your E-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter the Subject")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Please write a Message")]
+        public string Message { get; set; }
+    }
+}
